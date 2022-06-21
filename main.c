a727ace8350207d2d43931ce3c32c42d462a4571
Pablo Baldor <pablo.baldor91@gmail.com>
2022-06-21 01:51:57 -0300

    Paso 2 primer commit

--- main.c
+++ main.c
@@ -107,12 +107,176 @@ void menuResumenDelDia()
 
 }
 
+///////////////////PASO 2/////////////////////////////////////////
+//Prototipados Paso 2
+void obtenerRecetas(Receta[],int* );
+void obtenerDemanda(Preparacion[],int*);
+void obtenerStockIngredientes(StockIngrediente[],int*);
+int validarIngredientes(Receta,StockIngrediente [],int);
+void descontarIngredientesPorReceta(IngredienteXReceta[],int,StockIngrediente[],int);
+void descontarUnIngrediente(IngredienteXReceta,StockIngrediente[],int);
+int buscarRecetaPorNombre(char[],Receta[],int);
+void prepararDemandas(Preparacion[],int,Receta[],int,StockIngrediente[],int,PreparacionVenta[],int*);
+void registrarPreparadosVenta(PreparacionVenta[],int);
+void mostrarArregloDemandas(Preparacion[],int);
+void mostrarArregloRecetas(Receta[],int);
+void mostrarPreparadosVentas(PreparacionVenta[],int);
+
+//Implementaciones Paso 2
+
+void obtenerRecetas(Receta r[],int* rValidos)
+{
+    FILE* fp;
+    int i=0;
+    fp = fopen(RECETAS,"rb");
+    if(fp != NULL)
+    {
+        while(fread(&r[i],sizeof(Receta),1,fp) >0)
+        {
+            i++;
+        }
+        *rValidos = i;
+        fclose(fp);
+    }else
+        printf("No se pudo abrir el archivo recetas.bin");
+}
+void obtenerDemanda(Preparacion d[],int* dValidos)
+{
+    FILE* fp;
+    int i=0;
+    fp = fopen(DEMANDA,"rb");
+    if(fp != NULL)
+    {
+        while(fread(&d[i],sizeof(Preparacion),1,fp) >0)
+        {
+            i++;
+        }
+        *dValidos = i;
+        fclose(fp);
+    }else
+        printf("No se pudo abrir el archivo demanda.bin");
+}
+void obtenerPreparadosVentas(PreparacionVenta pv[],int* val)
+{
+    FILE* fp;
+    fp = fopen(PVENTAS,"rb");
+    int i=0;
+    while(i<*val && fread(&pv[i],sizeof(PreparacionVenta),1,fp) >0)
+    {
+        i++;
+    }
+
+    *val = i;
+
+    fclose(fp);
+}
+
+//Retorna un FALSO(0) si el stock no es suficiente para cubrir los ingredientes necesarios, en caso contrario devuelve un VERDADERO(1)
+int validarIngredientes(Receta receta,StockIngrediente stock[],int stockValidos)
+{
+    for(int ingredienteReceta=0; ingredienteReceta<receta.cantIngredientes; ingredienteReceta++)
+    {
+        for(int ingredienteStock=0; ingredienteStock<stockValidos; ingredienteStock++)
+        {
+            if(strcmpi(receta.ingredientes[ingredienteReceta].nombre_ingrediente,stock[ingredienteStock].nombre_ingrediente)==0 && receta.ingredientes[ingredienteReceta].cantidad > stock[ingredienteStock].cantidad)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+//Descuenta del stock, todos los ingredientes que se le pasen en un arreglo de ingredientes.
+void descontarIngredientesPorReceta(IngredienteXReceta ingredientesDeReceta[],int iValidos,StockIngrediente stock[],int sValidos)
+{
+    for(int ingrediente=0; ingrediente < iValidos; ingrediente++)
+    {
+        descontarUnIngrediente(ingredientesDeReceta[ingrediente],stock,sValidos);
+    }
+}
+
+//Descuenta un ingrediente del stock
+void descontarUnIngrediente(IngredienteXReceta ingrediente,StockIngrediente stock[],int stockValidos)
+{
+    for(int ingStock=0; ingStock < stockValidos; ingStock++)
+    {
+        if(strcmpi(ingrediente.nombre_ingrediente,stock[ingStock].nombre_ingrediente)==0)
+        {
+            stock[ingStock].cantidad -= ingrediente.cantidad;
+        }
+    }
+}
+
+//Retorna el indice donde se encuentra la receta que tiene el mismo nombre que la demanda
+int buscarRecetaPorNombre(char nombreReceta[],Receta recetas[],int rVal)
+{
+    int indice=0;
+    while(indice < rVal && strcmpi(recetas[indice].nombre_preparacion,nombreReceta)!=0)
+    {
+        indice++;
+    }
+    return indice;
+}
+//Prepara las demandas
+void prepararDemandas(Preparacion demandas[],int dValidos,Receta recetas[],int rValidos,StockIngrediente stock[],int sValidos,PreparacionVenta pVentas[],int* pvValidos)
+{
+    int indicePV=0;
+    int n=0;
+    int indiceReceta=0;
+    for(int i=0; i<dValidos; i++)
+    {
+        indiceReceta= buscarRecetaPorNombre(demandas[i].nombre_preparacion,recetas,rValidos);
+            strcpy(pVentas[indicePV].nombre_preparacion,demandas[i].nombre_preparacion);
+            while(validarIngredientes(recetas[indiceReceta],stock,sValidos) && n < demandas[i].cantidad)
+            {
+                descontarIngredientesPorReceta(recetas[indiceReceta].ingredientes,recetas[indiceReceta].cantIngredientes,stock,sValidos);
+
+                pVentas[indicePV].cantidad++;
+                n++;
+            }
+            n=0;
+        indicePV++;
+    }
+    *pvValidos = indicePV;
+}
+//Registra las preparaciones listas para la venta en el archivo "preparadosventa.bin"
+void registrarPreparadosVenta(PreparacionVenta pv[],int validosPV)
+{
+    FILE* fp;
+    fp = fopen(PVENTAS,"wb");
+
+    if(fp!=NULL)
+    {
+        for(int i=0; i<validosPV; i++)
+        {
+            fwrite(&pv[i],sizeof(PreparacionVenta),1,fp);
+        }
+        fclose(fp);
+    }
+}
 int main()
 {
+    Preparacion demandas[N];
+    int validosDem=0;
+    Receta recetas[N];
+    int validosRec=0;
     StockIngrediente stockIng[N];
     int validosIng=0;
+    PreparacionVenta preparacionesVenta[N];
+    int validosPV=0;
 
     obtenerStockIngredientes(stockIng,&validosIng);
 
+    //Inicio del paso 2
+    obtenerDemanda(demandas,&validosDem);
+    obtenerRecetas(recetas,&validosRec);
+    prepararDemandas(demandas,validosDem,recetas,validosRec,stockIngredientes,validosStock,preparacionesVenta,&validosPV);
+
+    registrarPreparadosVenta(preparacionesVenta,validosPV);
+    //Fin del paso 2
+
     return 0;
 }
+
+
